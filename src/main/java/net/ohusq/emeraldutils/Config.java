8efdfbef25212dbb5f8e492b3300a48b1ee75218
ohusq <mennom2007@gmail.com>
2024-11-14 19:55:11 +0100

    Refactored and fixed Config.java file

--- src/main/java/net/ohusq/emeraldutils/Config.java
+++ src/main/java/net/ohusq/emeraldutils/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.ohusq.emeraldutils;
 
 import java.util.List;
 import java.util.Set;
@@ -14,7 +14,7 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
-@EventBusSubscriber(modid = ExampleMod.MODID, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = EmeraldUtils.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class Config
 {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
@@ -55,9 +55,6 @@ public class Config
         magicNumber = MAGIC_NUMBER.get();
         magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
 
-        // convert the list of strings into a set of items
-        items = ITEM_STRINGS.get().stream()
-                .map(itemName -> BuiltInRegistries.ITEM.get(ResourceLocation.parse(itemName)))
-                .collect(Collectors.toSet());
+
     }
 }
